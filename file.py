a82fcd2c9a97f942c8a11418937e7450cfa4aedd
chirag shripat <118962745+chirag-shripat@users.noreply.github.com>
2023-06-27 21:14:45 +0530

    Create file.py

---
+++ file.py
@@ -0,0 +1,21 @@
+import unittest
+import requests
+
+class WebsiteTest(unittest.TestCase):
+    def test_website_loading(self):
+        url = 'https://www.youtube.com/'  # URL of the website to test
+
+        # Send a GET request to the website
+        response = requests.get(url)
+
+        # Check the status code of the response
+        if response.status_code == 200:
+            print("Website loaded successfully!")
+        else:
+            print("Website failed to load!")
+
+        # Assert that the status code is 200 (OK)
+        self.assertEqual(response.status_code, 200, "Website failed to load!")
+
+if __name__ == '__main__':
+    unittest.main()
